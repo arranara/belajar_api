65e5f3410db3517b9480dd2eedd46eca97f389b2
arranara <61440309+arranara@users.noreply.github.com>
2022-01-26 20:31:20 +0700

    Add files via upload

---
+++ conf/info.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+</head>
+<body>
+    <?php
+
+        $apikey = "2c8a549731b87255d70124d25c716406";
+
+        $sitename = "ZeroMovie";
+        $email = "r.fhlvi24@gmail.com";
+
+        $imgurl_1 = "http://image.tmdb.org/t/p/w500";
+        $imgurl_2 = "http://image.tmdb.org/t/p/w300";
+
+    ?>
+ 
+</body>
+</html>
